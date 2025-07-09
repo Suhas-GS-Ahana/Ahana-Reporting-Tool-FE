4027e717f41763c607c980ad1171c4e50b1cf338
Suhas-GS-Ahana <suhas.gs@ahanait.com>
2025-07-09 14:58:52 +0530

    execute page addes

--- src/app/temp-page/page.jsx
+++ src/app/temp-page/page.jsx
@@ -4,17 +4,22 @@ import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation';
 
 export default function TemPage() {
-    const id=9;
+    const id=13;
     const router = useRouter();
 
-    const handleClick = () => {
+    const handleEdit = () => {
         router.push(`/temp-page/temp-edit?id=${id}`);
+    }
+    const handleExecute = () => {
+        router.push(`/temp-page/temp-execute?id=${id}`);
     }
     
   return (
     <div>
         <h1>Edit button for ID: {id}</h1>
-        <Button onClick={handleClick}>Edit Process</Button>
+        <Button onClick={handleEdit}>Edit Process</Button>
+        <h1>Execute button for ID: {id}</h1>
+        <Button onClick={handleExecute}>Execute Process</Button>
     </div>
   )
 }
